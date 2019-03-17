8b94875cf8c0530c45faeb4991c4b535dd0de2cd
Granger Nicolas <Nicolas62x@gmail.com>
2019-03-17 15:16:27 +0100

    Déplacements-chargeAuto-avec coordonnées
    Bon j'ai tout mis dans un seul fichier car en plus d'être plus pratique, ne bug pas, si vous tenez aux multiples fichier je vous laisse séparer mon code, enfin bref
    Le robot est désormais capable de se déplacer a des coordonnées précises race a moveTo, il se charge de façon auto sur le chemins, j'ai du modifier la fonction de charge qui n'était pas répétable (ça marchait que la première fois)  il reste donc a dev  une fonction qui s'occupe de déplacer le robot d'îles de chaos en îles  de chaos et de le combattre à chaque fois

--- main.lua
+++ main.lua
@@ -2,48 +2,200 @@ local component = require("component")
 local computer = require("computer")
 local sides = require("sides")
 local robot = component.robot
-local nav = component.navigation
+local energyLevelToCharge = 19000
 
-print("Importing modules...")
 
--- Import modules
-local fileUtils = require("modules.fileUtils")
-local combatModule = require("modules.combatModule")
-local navModule = require("modules.navigationModule")
-local moveModule = require("modules.movementModule")
+local x=0
+local y=0
+local z=0
+local direction = 3
 
-navModule.setMovementModule(moveModule)
-moveModule.setReferences(navModule, robot, computer, sides)
+local checkEnergy
 
--- move, simpleMove, rotate, checkEnergy are now inside the "movementModule"
+local function moveForward(amount) -- Move the robot a certain amount of blocks forward while updating coords
+	local tmp = 0
+	for i = 1,amount,1 do
+		
+		if i-tmp == 50 then 
+			tmp = tmp + 50
+			checkEnergy() -- we should be checking energy when we move to make sure that we don't run out in long moves
+		end
+			
+		if robot.move(sides.front) == nil then 
+			
+		print("robot is stuck, insert whatever you want him to do here") 
+		
+		else if direction == 0 then -- updating his locals coords
+				x = x+1
+			else if direction == 1 then
+					z = z+1
+					else if direction == 2 then
+						x = x-1
+						else if direction == 3 then
+							z = z-1
+							
+						end
+					end
+				end
+			end
+		end
+	end
+end
 
-local function checkModules()
-    if fileUtils == nil then
-        print("Module 'fileUtils' could not be loaded. Please ensure that all modules are present inside 'home/modules/'")
-        os.exit()
-    end
-    if combatModule == nil then
-        print("Module 'combatModule' could not be loaded. Please ensure that all modules are present inside 'home/modules/'")
-        os.exit()
-    end
-    if navModule == nil then
-        print("Module 'navigationModule' could not be loaded. Please ensure that all modules are present inside 'home/modules/'")
-        os.exit()
-    end
-    if moveModule == nil then
-        print("Module 'movementModule' could not be loaded. Please ensure that all modules are present inside 'home/modules/'")
-    end
+local function moveUp(amount) -- Move the robot a certain amount of blocks up while updating coords
+	local tmp = 0
+	for i = 1,amount,1 do
+		
+		if i-tmp == 50 then 
+			tmp = tmp + 50
+			checkEnergy() -- we should be checking energy when we move to make sure that we don't run out in long moves
+		end
+			
+		if robot.move(sides.up) == nil then 
+			print("robot is stuck, insert whatever you want him to do here")
+		else
+			y = y+1
+		end
+	end
+end
+
+local function moveDown(amount) -- Move the robot a certain amount of blocks down while updating coords
+	local tmp = 0
+	for i = 1,amount,1 do
+		
+		if i-tmp == 50 then 
+			tmp = tmp + 50
+			checkEnergy() -- we should be checking energy when we move to make sure that we don't run out in long moves
+		end
+			
+		if robot.move(0) == nil then 
+			print("robot is stuck, insert whatever you want him to do here") 
+		else
+			y = y-1
+		end
+	end
+end
+
+local function rotateRight(amount) --rotate the robot amount time right
+	for i = 1,amount,1 do
+			
+		robot.turn(true)
+		
+		direction = direction + 1
+	
+		if direction > 3 then
+			direction = direction - 4
+		end
+	
+	end
+end
+
+local function rotateLeft(amount)
+	for i = 1,amount,1 do
+			
+		robot.turn(false)
+		
+		direction = direction - 1
+		
+		if direction < 0 then
+			direction = direction + 4
+		end
+	
+	end
+end
+
+local function rotateTo(dir)--rotate to a dir, please put a dir between 0 and 3  0 = east 1=south 2=west 3=north
+	while dir ~= direction do
+		rotateRight(1)
+	end
+end
+
+local function moveTo(pX,pY,pZ)--move robot to local coords pX pY pZ
+	
+	print("moving to:",pX,pY,pZ)
+	print("actual coords:",x,y,z)
+	
+	if pX>x then 
+		rotateTo(0)
+		moveForward(pX-x)
+	else if pX<x then
+			rotateTo(2)
+			moveForward(x-pX)
+		end
+	end
+	
+	if pY>y then
+		moveUp(pY-y)
+	else if pY<y then
+			moveDown(y-pY)
+		end
+	end
+	
+	if pZ>z then 
+		rotateTo(1)
+		moveForward(pZ-z)
+	else if pZ<z then
+			rotateTo(3)
+			moveForward(z-pZ)
+		end
+	end
+		
+end
+
+local charger = 1
+local solar = 2
+local redstone = 3
+
+checkEnergy = function () -- Check battery, if robot is low it places a charger
+    print("Checking energy...")
+	if computer.energy() < energyLevelToCharge then
+        print("Placing charger")
+		robot.select(charger)
+		robot.place(sides.front)
+		moveUp(1)
+		robot.select(solar)
+		robot.place(sides.front)
+		moveDown(2)
+		robot.select(redstone)
+		robot.place(sides.front)
+		moveUp(1)
+		rotateRight(1)
+		moveForward(1)
+		rotateLeft(1)
+		moveForward(1)
+        print("Charging...")
+		while (computer.energy() < computer.maxEnergy() - 200) do
+			os.sleep(1)
+            print("Charge level: " .. tostring(computer.energy()) .. "/" .. tostring(computer.maxEnergy() - 200)) -- Print charge progression.
+		end
+        print("Fully charged.")
+		rotateLeft(1)
+		robot.select(charger)
+		robot.swing(sides.front)
+		moveDown(1)
+		robot.select(redstone)
+		robot.swing(sides.front)
+		moveUp(2)
+		robot.select(solar)
+		robot.swing(sides.front)
+		moveDown(1)
+		rotateLeft(1)
+		moveForward(1)
+		rotateRight(1)
+		moveForward(1)
+		rotateRight(1)
+	end
 end
 
 local function init()
-    checkModules()
-    print("Localizing Chaos Guardian")
+    
+	print("Localizing Chaos Guardian")
     print("Subscribe to FuzeIII") -- In case the robot does not survive, "Subscribe to FuzeIII" will be its last words of wisdom
-	x,y,z = navModule.getCoords()
-	navModule.moveToWorldCenter()
-	moveModule.rotate(sides.east)
-
-	moveModule.move(sides.front, fileUtils.getNextGuardian(x))
+	
+	
+	moveTo(50,5,5)
+	
+	print("My pos x,y,z,dir: ",x,y,z,direction)
 end
 
 init()
